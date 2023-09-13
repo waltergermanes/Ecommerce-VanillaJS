0c4df03667d30ecf582c3f6a100a11683c6b33c9
Walter Germanes <78303625+waltergermanes@users.noreply.github.com>
2023-09-13 16:25:55 +0800

    first commit

---
+++ data/data.js
@@ -0,0 +1,212 @@
+export const data = [
+    {
+        id: 1,
+        name: "Air Jordan 1 Mid SE",
+        brand:"Nike",
+        category: "Shoes",
+        img:"./data/jordan-shoes/air-jordan-1-mid-se-shoes-p48zQ5.jpg",
+        price: 7595
+    },{
+        id: 2,
+        name: "Air Jordan 1 Elevate Low",
+        brand:"Nike",
+        category: "Shoes",
+        img:"./data/jordan-shoes/b51fd349-3ed2-493d-bd94-221534f79964.webp",
+        price: 7095
+    },{
+        id: 3,
+        name: "Tatum 1 St. Louis PF",
+        brand:"Nike",
+        category: "Shoes",
+        img:"./data/jordan-shoes/42cba1bc-3a7b-44df-a026-3c416d1d80b2.webp",
+        price: 6895
+    },{
+        id: 4,
+        name: "Zion 2 PF",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/e5feafbc-8beb-4b91-9ffd-ed14a62e1a67.webp",
+        price: 6895
+    },{
+        id: 5,
+        name: "Air Jordan XXXVII Low(Women BS)",
+        brand:"Nike",
+        category: "Shoes",
+        img:"./data/jordan-shoes/504e6e5b-6a2f-4f66-80af-f34bf82e9613.webp",
+        price: 10395
+    },{
+        id: 6,
+        name: "Air Jordan 13 Retro (MS)",   brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/air-jordan-13-retro-shoes-xcK6vg.jpg",
+        price:9695
+    },{
+        id: 7,
+        name: "Jordan Why Not .6 PF ms",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/8bd0693c-86a1-47ee-bcf6-99b3caeccef7.webp",
+        price: 8395
+    },{
+        id: 8,
+        name: "Air Jordan 1 Low WS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/7683c588-e0f5-46b9-9382-ef6ec80db821.webp",
+        price: 6195
+    },{
+        id: 9,
+        name: "Jordan Stadium 90 mS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/8ff9c769-7a52-4bec-aeaf-e1669372fe93.webp",
+        price: 7895
+    },{
+        id: 10,
+        name: "Jordan Series Mid mS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/77552c97-2a1b-4c7d-a8df-c71243cd8f3d.webp",
+        price: 5095
+    },{
+        id: 11,
+        name: "Air Jordan 1 Mid mS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/ea3a034a-351d-4d5e-9e39-6ebe24eebd23.webp",
+        price: 7095
+    },{
+        id: 12,
+        name: "Jordan One Take 4 PF mS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/b64ab483-f834-4847-a14f-d09d515420fd.webp",
+        price: 5295
+    },{
+        id: 13,
+        name: "Jordan Retro 6 G NRG mgS",
+        brand:"Nike",
+        category: "Shoes",
+         img:"./data/jordan-shoes/81c57e35-2f71-467d-89df-3b73fc8c3608.webp",
+        price: 11795
+    },{
+        id: 14,
+        name: "Incinerate Running Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Incinerate-Running-Shoes.webp",
+        price: 1680
+    },{
+        id: 15,
+        name: "INTERFLEX-Running-Shoes rs",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/INTERFLEX-Running-Shoes.webp",
+        price: 2100
+    },{
+        id: 16,
+        name: "Accent Running Shoes rs",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Accent-Running-Shoes.webp",
+        price: 1848
+    },{
+        id: 17,
+        name: "MB.01 Lo Basketball Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/MB.webp",
+        price: 6800
+    },{
+        id: 18,
+        name: "MB.02 Basketball Shoess",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/MB (1).webp",
+        price: 8500
+    },{
+        id: 19,
+        name: "Infusion Training Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Infusion-Training-Shoes.webp",
+        price: 5600
+    },{
+        id: 20,
+        name: "Infusion Training Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Infusion-Training-Shoes (1).webp",
+        price: 5600
+    },{
+        id: 21,
+        name: "Amare Running Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Amare-Running-Shoes.webp",
+        price: 6100
+    },{
+        id: 22,
+        name: "Aviator Running Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Aviator-Running-Shoes.webp",
+        price: 2520
+    },{
+        id: 23,
+        name: "Enzo Sport Men's Running Shoes",
+        brand:"Puma",
+        category: "Shoes",
+         img:"./data/Puma/shoes/Enzo-Sport-Men's-Running-Shoes.webp",
+        price: 2408
+    },{
+        id: 24,
+        name: "PUMA x ALEX TOUSSAINT Performance Tank Top Men",
+        brand:"Puma",
+        category: "Shirt",
+         img:"./data/Puma/shirt/PUMA-x-PERKS-AND-MINI-Graphic-Tee.jpg",
+        price: 3300
+    },{
+        id: 25,
+        name: "PUMA x PERKS AND MINI Graphic Tee",
+        brand:"Puma",
+        category: "Shirt",
+         img:"./data/Puma/shirt/PUMA-x-PERKS-AND-MINI-Graphic-Tee.webp",
+        price: 3300
+    },{
+        id: 26,
+        name: "PUMA x ALEX TOUSSAINT Performance Tank Top Men",
+        brand:"Puma",
+        category: "Shirt",
+         img:"./data/Puma/shirt/PUMA-x-ALEX-TOUSSAINT-Performance-Tank-Top-Men.webp",
+        price: 3300
+    },{
+        id: 27,
+        name: "PUMA x ALEX TOUSSAINT Performance Tank Top Men",
+        brand:"Puma",
+        category: "Shirt",
+         img:"./data/Puma/shirt/PUMA-x-ALEX-TOUSSAINT-Performance-Tank-Top-Men (1).webp",
+        price: 3300
+    },{
+        id: 28,
+        name: "Nike Sportswear Premium Essentials",
+        brand:"Nike",
+        category: "Shirt",
+         img:"./data/jordan-shoes/shirt/1fb267b2-2fac-48fd-9219-9139c522a47e.webp",
+        price: 3300
+    },{
+        id: 29,
+        name: "Nike Dri-FIT Miler",
+        brand:"Nike",
+        category: "Shirt",
+         img:"./data/jordan-shoes/shirt/81d8e4d1-8ac4-4661-96a1-84fa8644b887.webp",
+        price: 3300
+    },{
+        id: 30,
+        name: "Nike Sportswear Tech Pack",
+        brand:"Nike",
+        category: "Shirt",
+         img:"./data/jordan-shoes/shirt/fc519f24-97e9-4a16-8b6a-670f0b759d7f.webp",
+        price: 3300
+    }
+]
---
+++ index.js
@@ -0,0 +1,208 @@
+import { data } from "./data/data.js"
+
+const brand = document.querySelector('.brand')
+const categoryList = document.querySelector('.categorylist')
+const range = document.getElementById('range')
+const price = document.getElementById('price')
+const container = document.getElementById('main')
+const clearFilter = document.querySelector('.clearfilter')
+const item = document.querySelector('.item')
+const noFound = document.querySelector('.no_found')
+const search_product = document.querySelector('.search_product')
+const cartContainer = document.getElementById('cartContainer')
+const cartCount = document.querySelector('.cartCount')
+const total = document.getElementById('total')
+noFound.innerText = data.length
+item.innerText = `All`
+
+const product = (data) =>{ 
+              return  container.innerHTML =  data.map((p) =>{
+                      return`<div class="card" alt="${p.name}">
+                              <img src="${p.img}" class="product_img">
+                                  <div class="product_info">
+                                      <span class="product_brand">${p.name.substring(0, 17)}...</span>
+                                      <span class="product_price">${p.price.toLocaleString("en-US", {style: "currency", currency: "PHP"})}</span>
+                                  </div>
+                                  <button onclick="addToCart(${p.id})" class="add_to_cart">Add to cart</button>
+                          </div> `     
+                        }).join("")      
+                        }
+
+const categoriesFiltered = (c) =>{
+  categoryList.innerHTML = Array.from(new Set(c.map((c) =>{
+      return `<span class="category">${c.category}</span><hr>`
+    }))).join("")
+
+  categoryList.addEventListener("click", (e)=>{
+    const selectedByCategory = e.target.textContent
+    const filteredselectedByCategory = data.filter(p => p.category === selectedByCategory)
+    const selectedByAll = filteredselectedByCategory.filter(p => p.brand === brand.value)
+   
+    if(brand.value === "All"){
+      product(data.filter(c=> c.category === selectedByCategory))
+      noFound.innerText = filteredselectedByCategory.length
+      item.innerText = `of "All ${selectedByCategory}"`
+    }
+    if(brand.value !== "All" && item.innerText !== "All"){
+      product(selectedByAll)
+      noFound.innerText = selectedByAll.length
+      item.innerText = `of "${brand.value} ${selectedByCategory}"`
+    }
+  })
+}
+const f =() =>{
+    categoriesFiltered(data)
+    product(data)
+    item.innerText = `All`
+    noFound.innerText = data.length
+}
+const brands = () =>{
+    const brandList = Array.from(new Set(data.map(b => b.brand)))
+    const allBrand = ["All",...brandList.map(b =>b )]
+
+    brand.innerHTML = allBrand.map(b =>{
+      return `<option value=${b}>${b}</option>`
+    }).join("")
+
+    brand.addEventListener("change", (e)=>{
+        const selectedByBrand = e.target.value
+        const filterBrand = data.filter(p => p.brand === selectedByBrand)
+   
+        if(selectedByBrand === "All" ){
+          return f()
+        } 
+        product(filterBrand.filter(p => p.price <= price.innerText))
+        categoriesFiltered(data.filter(p => p.brand === selectedByBrand))
+        item.innerText = `of "${selectedByBrand}"`
+        noFound.innerText = filterBrand.length
+     })}
+
+clearFilter.addEventListener("click", ()=>{
+    product(data)
+    categoriesFiltered(data)
+    noFound.innerText = data.length
+    item.innerText = `All`
+    brand.value = "All"
+})
+
+const priceList = () =>{
+    const pList = Array.from(new Set(data.map(p => p.price)))
+    const minPrice = Math.min(...pList)
+    const maxPrice = Math.max(...pList)
+
+   range.value = maxPrice
+    price.innerText = maxPrice
+ 
+    range.addEventListener("change",(e)=>{
+      const valPrice = e.target.value
+     
+      range.max = maxPrice
+      range.min = minPrice
+      price.innerText = valPrice
+  
+      const filterByBrand = data.filter(p => p.brand === brand.value)
+      if(brand.value === "All" && item.innerText === "All")product(data.filter(p => p.price <= price.innerText))
+      if(brand.value === "All" && item.innerText !== "All")product(data.filter(p => p.price <= price.innerText))
+      if(brand.value !== "All" && item.innerText !== "All")product(filterByBrand.filter(p => p.price <= price.innerText))
+    })
+}
+search_product.addEventListener("keyup", e =>{
+  const searchByName = e.target.value.toLowerCase()
+  product(data.filter(p => p.name.toLowerCase().includes(searchByName)))
+ })
+
+let myCart = []   
+const addToCart = (id)=>{
+  let searchByID = myCart.find(x => x.id === id)
+  if(searchByID === undefined){
+      myCart.push({
+        id,
+        qty: 1
+      })
+  }else{
+      searchByID.qty +=1
+  }
+  loadCart(myCart)
+  update()
+}
+const decrement = (id)=>{
+  let searchByID = myCart.find(x => x.id === id)
+
+  if(searchByID.qty === 1){
+      return loadCart(myCart.filter(c=> c.id !== id))
+  }else{
+      searchByID.qty -=1
+  }
+  update()
+ 
+  loadCart(myCart)
+}
+let allTotal =''
+const update=()=>{
+  let countItem = myCart.map(x => x.qty).reduce((x, y) => x + y, 0)
+  let searchid = myCart.map(d=> {
+    const { id, qty } = d
+    let search = data.find(y => y.id === id) || []
+    return qty * search.price
+  })
+   allTotal = searchid.reduce((x, y) => x + y, 0)
+  cartCount.innerText = countItem
+  total.innerText = allTotal.toLocaleString("en-US", {style: "currency", currency: "PHP"})
+  
+}
+
+if (myCart.length === 0) {
+   cartContainer.innerHTML = `<h2 class="empty">Cart is Empty</h2>`
+}
+
+
+const loadCart = (cart) =>{
+  console.log(cart, 9)
+  if (cart.length === 0) {
+    return cartContainer.innerHTML = `<h2 class="empty">Cart is Empty</h2>`
+  } else {
+    return cartContainer.innerHTML = cart.map((c, cid)=>{
+      let search = data.find(y=> y.id === c.id) || []
+      let res = search.price * c.qty
+      return `<div class="item">
+                <div class="pic_name">
+                    <img src="${search.img}" class = "cart_img" alt="">
+                </div>
+                <div class="price">
+                    <h4 id="brand_name">${search.name.substring(0, 20)}...</h4>
+                    <h4 id="price_per_item">Price: ${search.price.toLocaleString("en-US", {style: "currency", currency: "PHP"})}</h4>
+                    <h4 id="qty">Qty: ${c.qty}</h4>
+                    <p id="price2">${res.toLocaleString("en-US", {style: "currency", currency: "PHP"})}</p>
+                </div>
+                <div class="add_remove">
+                        <button id="remove" onclick='decrement(${c.id})' class="button_cart">-</button>
+                        <button id="add" onclick='addToCart(${c.id})' class="button_cart">+</button>
+                </div>
+                  </div>
+                <hr>`
+              }).join("")}
+            }
+document.querySelector('.removeAll').addEventListener('click', ()=>{
+  let text= "Press OK or Cancel"
+  if(confirm(text) == true && myCart.length !== 0){
+    loadCart(myCart = [])
+    cartCount.innerText = 0
+    total.innerText = 0
+  }
+})
+document.querySelector('.btnCheckout').addEventListener('click', ()=>{
+   if (myCart.length) {
+       const totalPayment = allTotal.toLocaleString("en-US", {style: "currency", currency: "PHP"})
+      alert(`Your total payment is ${totalPayment}`)
+   }
+})
+console.log('first')
+window.product = product
+const card = document.querySelector('.card')
+console.log(card)
+window.addToCart = addToCart
+window.decrement = decrement
+product(data)
+priceList()
+brands()
+categoriesFiltered(data)
